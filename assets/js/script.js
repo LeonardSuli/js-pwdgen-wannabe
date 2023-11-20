381c36d3d796fc7c72c36a8911454b03d3739fce
LeonardSuli <leonardsuli1991@gmail.com>
2023-11-20 18:59:00 +0100

    fixed number in string and moved in file js

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+//chiedi all'utente il suo nome
+const name = prompt('What\'s your name?');
+console.log(name);
+
+//chiedi all'utente il cognome
+const surname = prompt('What\'s your surname?');
+console.log(surname);
+
+//chiedi all'utente il suo colore preferito
+const color = prompt('What\'s your favourite color?');
+console.log(color);
+
+//scrivi sulla pagina nomecognomecolorepreferito23
+document.writeln(name + surname + color + '23');
+
+console.log(name + surname + color + '23');
